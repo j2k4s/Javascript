4c514fafc02b9d33bc3b549319af379698bd7e4e
j2k4s <joycekellydasilva17@gmail.comgit config --global user.name j2k4sgit config --global user.email joycekellydasilva17@gmail.com>
2022-10-23 16:44:45 -0300

    meu primeiro commit

---
+++ VARIAVEIS E OPERADORES/aula.js
@@ -0,0 +1,15 @@
+let soma = 10;
+soma += 20;
+let subtracao = 10;
+subtracao -= 10;
+let multiplicacao = 10;
+multiplicacao *= 5;
+let divisao = 10;
+divisao /= 2;
+
+console.log('soma é:' + soma, '.','subtração é:' + subtracao, '.', 'multiplicacao é:' + multiplicacao, '.', 'divisao é:' + divisao, '.' );
+
+
+
+
+
---
+++ VARIAVEIS E OPERADORES/desafio.js
@@ -0,0 +1,7 @@
+const precoCombustivel = 4.59;
+const gastoMedio = 10;
+const distancia = 50;
+
+let calculo = distancia/gastoMedio*precoCombustivel;
+
+console.log('Valor gasto é:' + ' ' + calculo);
