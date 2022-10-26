9a9b05f9663a78422e91e621ded7ab560347f7fb
j2k4s <joycekellydasilva17@gmail.comgit config --global user.name j2k4sgit config --global user.email joycekellydasilva17@gmail.com>
2022-10-25 21:00:49 -0300

    atualilações

---
+++ FUNÇÕES/aula.js
@@ -0,0 +1,6 @@
+ function incrementarJuros (valor, percentualJuros) {
+    const valorDeAcrecimo = (percentualJuros / 100) * valor;
+    return valor + valorDeAcrecimo;
+}
+
+console.log(incrementarJuros(100, 10)); 
