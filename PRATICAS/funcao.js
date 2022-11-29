f0ac6f9881550a97af5257231100ffb470167a13
j2k4s <joycekellydasilva17@gmail.comgit config --global user.name j2k4sgit config --global user.email joycekellydasilva17@gmail.com>
2022-11-29 20:49:20 -0300

    atualizações

---
+++ PRATICAS/funcao.js
@@ -0,0 +1,24 @@
+/*function escrevaMeuNome(name){
+    console.log('Meu nome é ' + name);
+}
+escrevaMeuNome('Joyce');
+escrevaMeuNome('Bruna');
+
+function calcularIdade(idade){
+    if(idade >= 18){
+        console.log('Você é maior de idade');
+    }else{
+        console.log('Você é menor de idade');
+    }
+}
+
+calcularIdade(18);
+*/
+
+function calcularDesconto(valor, desconto){
+    if (formaDePagamento === 1) {
+        console.log( precoDoProduto - (precoDoProduto * 0.1));
+     } else if (formaDePagamento === 2 ) {
+         console.log( precoDoProduto - (precoDoProduto * 0.15));
+}
+}
